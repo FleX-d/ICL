20acdbe34ee357523ba780bb209b3db41b61717b
Not your problem <tlovisek@gmail.com>
2018-06-19 14:51:59 +0200

    Update build.sh (#7)

--- gen/build.sh
+++ gen/build.sh
@@ -1,11 +1,69 @@
 #!/bin/bash
 
+changeDirection()
+{
+  echo "changing direction" 
+  
+  cp $file "${file}.new"
+  workFile="${file}.new"
+  
+  dirFromNew=$(echo $directionFrom | sed 's/from/to/g')
+  #echo $dirFromNew
+
+  dirToNew=$(echo $directionTo | sed 's/to/from/g')
+  #echo $dirToNew
+  
+  #replace from to new in element
+  sed -i "s|$directionFrom|\t$dirFromNew|g" $workFile
+  sed -i "s|$directionTo|\t$dirToNew|g" $workFile
+}
+
+changeFunctions()
+{
+  while read -r function; do
+    echo "$function" | fgrep -q "in"
+    if [ $? -eq 0 ]
+    then
+      functionNew=$(echo $function | sed 's/in/out/g')
+    else
+      functionNew=$(echo $function | sed 's/out/in/g')
+    fi
+    sed -i "s|$function|$functionNew|g" $workFile
+    
+  done < <(cat $workFile | fgrep "function name=")
+}
+
+#############
+####START####
+#############
+param=$1
 CATALOGUE="./catalogue.xml"
 
+echo "*** start ***"
+
+argc=$#
+argv=($@)
+
 echo "<colection>" > $CATALOGUE
-for arg in "$@"
-do
-    echo "    <file name=\"$arg\"/>" >> $CATALOGUE
+#check all files
+for (( j=1; j<argc; j++ )); do
+  file=${argv[j]}
+  echo "check file: $file"
+  directionFrom=`cat $file | fgrep name="\"from\""`
+  directionTo=`cat $file | fgrep name="\"to\""`
+
+  echo $directionFrom | fgrep $param
+
+  if [ $? -eq 1 ]
+  then
+    changeDirection
+    changeFunctions
+    cp $workFile $file
+    rm $workFile
+    echo "    <file name=\"$file\"/>" >> $CATALOGUE
+  else
+    echo "$file: direction ok"
+  fi
 done
 echo "</colection>" >> $CATALOGUE
 
